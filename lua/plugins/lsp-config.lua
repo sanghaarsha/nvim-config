b63e92490692f884f6adbde3cedb0d20abf11223
sangharsha <sanghaarsha@gmail.com>
2025-01-22 12:14:10 +0000

    setup completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,34 +1,47 @@
 return {
   {
-  "williamboman/mason.nvim",
-  config = function()
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
       require("mason").setup()
-  end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"cssls","html","lua_ls","ts_ls","intelephense", "sqls","volar","tailwindcss"}
+        ensure_installed = {
+          "cssls",
+          "html",
+          "lua_ls",
+          "ts_ls",
+          "intelephense",
+          "sqls",
+          "volar",
+          "tailwindcss",
+        },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.cssls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.intelephense.setup({})
-      lspconfig.sqls.setup({})
-      lspconfig.volar.setup({})
-      lspconfig.tailwindcss.setup({})
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.intelephense.setup({ capabilities = capabilities })
+      lspconfig.sqls.setup({ capabilities = capabilities })
+      lspconfig.volar.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
