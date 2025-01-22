ab0deba514ba7dafd8965ac0331b4dc89883832d
sangharsha <sanghaarsha@gmail.com>
2025-01-22 09:51:01 +0000

    setup lspconfig for all lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,11 +17,18 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require('lspconfig')
+      lspconfig.cssls.setup({})
+      lspconfig.html.setup({})
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.intelephense.setup({})
+      lspconfig.sqls.setup({})
+      lspconfig.volar.setup({})
+      lspconfig.tailwindcss.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
-  } 
+  }
 }
