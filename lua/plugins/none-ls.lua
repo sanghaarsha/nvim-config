78d1c2748564f739ada5da16bf34888a9f30e84c
sangharsha <sanghaarsha@gmail.com>
2025-01-22 10:38:44 +0000

    setup stylua, eslint and prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,10 @@ return {
 
     null_ls.setup({
       sources = {
+        null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint")
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
